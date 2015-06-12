efadb22c73ffda6735efdf44892e355b906b4bb9
goolomb <martin.golomb@seznam.cz>
2015-06-13 00:47:27 +0200

    pridany testovaci tridy

---
+++ test/dtdtoxmlschemaconverter/ContentManagerTest.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package dtdtoxmlschemaconverter;
+
+import org.junit.Test;
+
+/**
+ *
+ * @author Goolomb
+ */
+public class ContentManagerTest {
+    
+    /**
+     * Test of extractDTD method, of class ContentManager.
+     */
+    @Test
+    public void testExtractDTD() throws Exception {
+        //TODO
+    }
+
+    /**
+     * Test of createXMLSchema method, of class ContentManager.
+     */
+    @Test
+    public void testCreateXMLSchema() {
+        //TODO ale asi zrusim
+    }
+
+}

8330cd5600d3f87c0766dadfc377c34ded65feb4
mmarky27 <umlauf.marketa@gmail.com>
2015-06-12 18:22:36 +0200

    implementace Mainu (bude upraveno)

--- src/dtdtoxmlschemaconverter/Main.java
+++ src/dtdtoxmlschemaconverter/Main.java
@@ -6,16 +6,33 @@
 
 package dtdtoxmlschemaconverter;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Patrik
  */
 public class Main {
-
+    static String path;
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        try {
+            System.out.println("Please enter .xml file path: ");
+            path = sc.nextLine();  
+            System.out.println("You entered: " + path);
+            
+        } catch (NullPointerException ex) {
+            throw new NullPointerException("error");
+        }
+        
+        //String extracted = ContentManager.extractDTD(path);
+        //String xmlSchema = Converter.assembleXMLSchema(Converter.parseDTD(extracted));
+        //vytvoreni souboru .xsd:
+        ContentManager.createXMLSchema("<schema><taktak></taktak></schema>"); //pak tam bude xmlSchema string
+        //ContentManager.saveFile(path);
+        
     }
 }

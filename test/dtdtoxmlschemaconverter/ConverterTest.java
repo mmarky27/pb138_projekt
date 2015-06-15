605801c0e5a0fdc80a9b3bfe6798ce2ed99406d6
goolomb <martin.golomb@seznam.cz>
2015-06-15 22:33:02 +0200

    spraveny test splitContent

--- test/dtdtoxmlschemaconverter/ConverterTest.java
+++ test/dtdtoxmlschemaconverter/ConverterTest.java
@@ -74,6 +74,27 @@ public class ConverterTest {
      */
     @Test
     public void testAssembleXMLSchema() {
+        String expected = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" + s
+            + "<schema targetNamespace=\"http://www.w3.org/2001/XMLSchema\" xmlns=\"http://www.w3.org/2001/XMLSchema\" elementFormDefault=\"qualified\">" + s
+            + "<element name=\"zamestnanec\" >" + s
+            + "<complexType>" + s
+            + "<sequence>" + s
+            + "<element ref=\"jmeno\" />" + s
+            + "<element ref=\"narozen\" />" + s
+            + "</sequence>" + s
+            + "<attribute name=\"id\" type=\"string\" use=\"required\" />" + s
+            + "<attribute name=\"plat\" type=\"string\" use=\"optional\" />" + s
+            + "<attribute name=\"pohlavi\" use=\"optional\">" + s
+            + "<simpleType>" + s
+            + "<restriction base=\"string\">" + s
+            + "<enumeration value=\"muz \" />" + s
+            + "<enumeration value=\"zena \" />" + s
+            + "<enumeration value=\"jine\" />" + s
+            + "</restriction>" + s
+            + "</simpleType>" + s
+            + "</attribute>" + s
+            + "</complexType>" + s
+            + "</element>" + s;
         String result = Converter.assembleXMLSchema(elements);
         String result2 = Converter.assembleXMLSchema(DTDParser.output(dtdToParse));
         String result3 = Converter.assembleXMLSchema(DTDParser.output(dtdToParse2));
@@ -81,11 +102,10 @@ public class ConverterTest {
 
         //TODO
         System.out.println(result);
-        System.out.println("here");
-        /*assertEquals(expected, result);
-        assertEquals(expected, result2);
-        assertTrue(!expected.equals(result3));
-        */
+        assertEquals(expected, result);
+        //assertEquals(expected, result2);
+        //assertTrue(!expected.equals(result3));
+        
         
         //TODO assemblovani s entitama nebo notation
     }
@@ -96,87 +116,101 @@ public class ConverterTest {
             + "<complexType>" + s
             + "<sequence>" + s
             + "<element ref=\"jmeno\" />" + s
-            + "<element ref=\"narozen\" />"+ s
+            + "<element ref=\"narozen\" />" + s
             + "</sequence>" + s
             + "<attribute name=\"id\" type=\"string\" use=\"required\" />" + s
-            + "<attribute name=\"plat\" type=\"string\" />" + s
-            + "<attribute name=\"pohlavi\" type=\"string\" >" + s
-               + "<simpleType>" + s
-                    + "<restriction>" + s
-                        + "<enumeration value=\"check\">" + s
-                        + "<enumeration value=\"cash\">" + s
-                    + "</restriction>" + s
-                + "</simpleType>" + s
+            + "<attribute name=\"plat\" type=\"string\" use=\"optional\" />" + s
+            + "<attribute name=\"pohlavi\" use=\"optional\">" + s
+            + "<simpleType>" + s
+            + "<restriction>" + s
+            + "<enumeration value=\"muz \" />" + s
+            + "<enumeration value=\"zena \" />" + s
+            + "<enumeration value=\"jine\" />" + s
+            + "</restriction>" + s
+            + "</simpleType>" + s
             + "</attribute>" + s
             + "</complexType>" + s
-            + "</element>" + s;
+            + "</element>" + s + s;
         Method method = Converter.class.getDeclaredMethod("assembleElem", new Class[]{DTDObject.class, StringBuilder.class});
         method.setAccessible(true);
         
-        String result  = (String) method.invoke(new Converter(), new java.lang.Object[]{elements.get(0), new StringBuilder()});
-        assertEquals(expected, result);
+        StringBuilder sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{elements.get(0), sb});
+        assertEquals(expected, sb.toString());
 
-        expected = "<element name=\"chleba\" type=\"string\" />" + s;
-        result  = (String) method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "(#PCDATA)"), new StringBuilder()});
-        assertEquals(expected, result);
+        sb = new StringBuilder();
+        expected = "<element name=\"chleba\" type=\"string\" />" + s + s;
+        method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "#PCDATA"), sb});
+        assertEquals(expected, sb.toString());
         
-        expected = "<element name=\"chleba\" />" + s;
-        result  = (String) method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "(EMPTY)"), new StringBuilder()});
-        assertEquals(expected, result);
+        expected = "<element name=\"chleba\" />" + s + s;
+        sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "EMPTY"), sb});
+        assertEquals(expected, sb.toString());
 
         expected = "<element name=\"chleba\" >" + s
             + "<complexType>" + s
             + "<sequence>" + s
             + "<any minOccurs=\"0\" />" + s
-            + "</sequence>"
+            + "</sequence>" + s
             + "</complexType>" + s
-            + "</element>" + s;
-        result  = (String) method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "(ANY)"), new StringBuilder()});
-        assertEquals(expected, result);
+            + "</element>" + s + s;
+        sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{new DTDObject("chleba", ObjectType.ELEMENT, "(ANY)"), sb});
+        assertEquals(expected, sb.toString());
     }
 
     @Test
     public void testAssembleAttr() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException { //returns String
-        String expected = "<attribute name=\"plat\" type=\"string\">";
-        Method method = Converter.class.getDeclaredMethod("assembleAttr", new Class[]{Attribute.class});
+        String expected = "<attribute name=\"plat\" type=\"string\" use=\"optional\" />" + s;
+        Method method = Converter.class.getDeclaredMethod("assembleAttr", new Class[]{String.class, String.class, StringBuilder.class});
         method.setAccessible(true);
+
+        StringBuilder sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{ attributes.get(1).getName(), attributes.get(1).getContent(), sb});
+        assertEquals(expected, sb.toString());
         
-        String result = (String) method.invoke(new Converter(), new java.lang.Object[]{ attributes.get(1)});
-        assertEquals(expected, result);
+        expected = "<attribute name=\"id\" type=\"string\" use=\"required\" />" + s;
+        sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{ attributes.get(0).getName(), attributes.get(0).getContent(), sb});
+        assertEquals(expected, sb.toString());
         
-        expected = "<attribute name=\"id\" type=\"string\" use=\"required\">";
-        result = (String) method.invoke(new Converter(), new java.lang.Object[]{ attributes.get(0)});
-        assertEquals(expected, result);
-        
-        result = (String) method.invoke(new Converter(), new java.lang.Object[]{ new Attribute("payment", "type", "(check|cash) \"cash\"")});
-        expected = "<attribute name=\"type\" type=\"string\" default=\"cash\">" + s
-                + "<simpleType>" + s
-                + "<restriction>" + s
-                + "<enumeration value=\"check\"/>" + s
-                + "<enumeration value=\"cash\"/>" + s
-                + "</restriction>" + s
-                + "</simpleType>" + s
-                + "</attribute>" + s;
-        assertEquals(expected, result);
+        sb = new StringBuilder();
+        method.invoke(new Converter(), new java.lang.Object[]{ "payment", "(check|cash) \"cash\"", sb});
+        expected = "<attribute name=\"payment\" default=\"cash\">" + s
+            + "<simpleType>" + s
+            + "<restriction base=\"string\">" + s
+            + "<enumeration value=\"check\" />" + s
+            + "<enumeration value=\"cash\" />" + s
+            + "</restriction>" + s
+            + "</simpleType>" + s
+            + "</attribute>" + s;
+        System.out.println(sb.toString());
+        assertEquals(expected, sb.toString());
     }
 
     @Test
     public void testAssembleAttrs() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
-        String expected = "<attribute name=\"plat\" type=\"string\">" + s
-                + "<attribute name=\"id\" type=\"string\" use=\"required\">";
+        String expected = "<attribute name=\"id\" type=\"string\" use=\"required\" />" + s
+            + "<attribute name=\"plat\" type=\"string\" use=\"optional\" />" + s;
         Method method = Converter.class.getDeclaredMethod("assembleAttrs", new Class[]{List.class, StringBuilder.class});
         method.setAccessible(true);
         
         StringBuilder sb = new StringBuilder();
         method.invoke(new Converter(), new java.lang.Object[]{ attributes.subList(0, 2),sb });
-        
         assertEquals(expected, sb.toString());
     }
     
     @Test
-    public void testAssembleAttrContent() {
-        fail();
-        //TODO
+    public void testAssembleAttrContent() throws NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        //String expected = "" + s;
+        Method method = Converter.class.getDeclaredMethod("assembleAttrContent", new Class[]{String.class, StringBuilder.class});
+        method.setAccessible(true);
+        StringBuilder sb = new StringBuilder();
+        String result = (String) method.invoke(new Converter(), new java.lang.Object[]{"\"defaultPotato\"", sb});        
+        System.out.println(result);
+        //assertEquals(expected, sb.toString());
+        
     }
     
     @Test
@@ -207,7 +241,7 @@ public class ConverterTest {
         method.invoke(new Converter(), new java.lang.Object[]{sb, "(nazev, autor)+"});
         assertEquals(expected, sb.toString());
         
-        expected = "<sequence minOccurs=\"0\">" + s
+        expected = "<sequence  minOccurs=\"0\">" + s
             + "<element ref=\"nazev\" />" + s
             + "</sequence>" + s;
         sb = new StringBuilder();
@@ -218,7 +252,8 @@ public class ConverterTest {
             + "<element ref=\"nazev\" />" + s
             + "</all>" + s;
         sb = new StringBuilder();
-        method.invoke(new Converter(), new java.lang.Object[]{sb, "(#PCDATA | nazev)"});
+        method.invoke(new Converter(), new java.lang.Object[]{sb, "(#PCDATA | nazev)*"});
+        System.out.println(sb.toString());
         assertEquals(expected, sb.toString());
     }
 
@@ -226,14 +261,15 @@ public class ConverterTest {
     public void testSplitContent() throws SecurityException, NoSuchMethodException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
         Method method = Converter.class.getDeclaredMethod("splitContent", new Class[]{String.class, String.class, String.class, String.class});
         method.setAccessible(true);
-        List<String> result = (ArrayList<String>) method.invoke(new Converter(), new java.lang.Object[]{"(potato, egg, cheese)", ",", "(", ")"});
-        List<String> result2 = (ArrayList<String>) method.invoke(new Converter(), new java.lang.Object[]{"(potato | (egg, cheese))", "|", "(", ")"});
-                
+        
+        List<String> result = (ArrayList<String>) method.invoke(new Converter(), new java.lang.Object[]{"(potato, egg), cheese", ",", "(", ")"});
+        List<String> result2 = (ArrayList<String>) method.invoke(new Converter(), new java.lang.Object[]{"potato | (egg, cheese) | bacon", "|", "(", ")"});
+        
         assertNotNull(result);
-        assertEquals(3, result.size());
+        assertEquals(2, result.size());
         
         assertNotNull(result2);
-        assertEquals(2, result2.size());
+        assertEquals(3, result2.size());
     }
 
     @Test
